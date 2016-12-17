b87f10f5d6609d794541a09bd59dadc4a19c2093
你的名字 <email@xxx.xxx>
2016-12-17 21:18:57 +0800

    add posts nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-    resources :groups
+    devise_for :users
+    resources :groups do
+        resources :posts
+    end
     root 'groups#index'
 end
 
